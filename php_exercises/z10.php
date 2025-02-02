7667d6c8f2d8daa047c6769d33ee573357e0b8af
mayten10000 <106926459+mayten10000@users.noreply.github.com>
2025-02-02 17:35:53 +0300

    php added

---
+++ php_exercises/z10.php
@@ -0,0 +1,18 @@
+<?php
+
+function sieve(n){
+    s = [];
+    s[1] = 0;
+
+    for (k = 2; k <= n; k++){
+        s[k] = 1;
+    }
+
+    for (k = 2; k <= sqrt(n); k++){
+        if (s[k] == 1){
+            for (l = k*k )
+        }
+    }
+}
+
+?>
