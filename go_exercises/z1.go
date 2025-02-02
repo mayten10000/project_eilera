0a8a77ea07d1b3320010458ffa9c5b4a0549619a
mayten10000 <106926459+mayten10000@users.noreply.github.com>
2025-02-02 17:40:43 +0300

    go added

---
+++ go_exercises/z1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	limit := 1000
+	sum := 0
+
+	for i := 0; i < limit; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+
+	fmt.Println("Сумма ", sum)
+
+}
